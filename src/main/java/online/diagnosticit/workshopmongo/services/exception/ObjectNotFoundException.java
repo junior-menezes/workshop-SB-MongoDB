ad219a6e9475b9c6a1748394a336b256e918375c
junior-menezes <casagrandejuniorm@gmail.com>
2021-05-06 19:41:04 -0300

    Tratamento de erro personalizado e obtendo usuário por ID

---
+++ src/main/java/online/diagnosticit/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package online.diagnosticit.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
