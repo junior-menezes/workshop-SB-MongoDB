f4b63e4a8e3ae1e16933b692eda36eae12ced8f4
junior-menezes <casagrandejuniorm@gmail.com>
2021-05-10 20:50:44 -0300

    Criados metodos para Deletar usuário com DELETE - 2

--- src/main/java/online/diagnosticit/workshopmongo/resources/UserResource.java
+++ src/main/java/online/diagnosticit/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -54,5 +55,13 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable String id, @RequestBody UserDTO objDto){
+		objDto.setId(id);
+		User obj = service.fromDTO(objDto);
+		UserDTO userDto = new UserDTO(service.update(obj));
+		return ResponseEntity.ok().body(userDto);
 	}
 }
