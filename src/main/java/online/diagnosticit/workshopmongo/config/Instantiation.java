571222d25a21f5523ea3eb47e05b4a2635afbdbb
junior-menezes <casagrandejuniorm@gmail.com>
2021-05-11 11:40:23 -0300

    Criado Entidade Post com User aninhado

--- src/main/java/online/diagnosticit/workshopmongo/config/Instantiation.java
+++ src/main/java/online/diagnosticit/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package online.diagnosticit.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import online.diagnosticit.workshopmongo.domain.Post;
 import online.diagnosticit.workshopmongo.domain.User;
+import online.diagnosticit.workshopmongo.repository.PostRepository;
 import online.diagnosticit.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,8 +18,15 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		//para zerar as instanciações que já estão no banco e iniciarmos os testes com ele limpo.
 		userRepository.deleteAll(); 
@@ -24,7 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob)); 
+		Post post1 = new Post(null, sdf.parse("11/05/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("10/05/2021"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
---
+++ src/main/java/online/diagnosticit/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package online.diagnosticit.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import online.diagnosticit.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
