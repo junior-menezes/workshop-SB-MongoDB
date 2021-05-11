8a159899a891ac2903f926a567341c187148439f
junior-menezes <casagrandejuniorm@gmail.com>
2021-05-11 11:56:06 -0300

    Projetando Post no MongoDB com AuthorDTO

--- src/main/java/online/diagnosticit/workshopmongo/config/Instantiation.java
+++ src/main/java/online/diagnosticit/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import online.diagnosticit.workshopmongo.domain.Post;
 import online.diagnosticit.workshopmongo.domain.User;
+import online.diagnosticit.workshopmongo.dto.AuthorDTO;
 import online.diagnosticit.workshopmongo.repository.PostRepository;
 import online.diagnosticit.workshopmongo.repository.UserRepository;
 
@@ -29,16 +30,19 @@ public class Instantiation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		//para zerar as instanciações que já estão no banco e iniciarmos os testes com ele limpo.
-		userRepository.deleteAll(); 
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("11/05/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("10/05/2021"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("11/05/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("10/05/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
