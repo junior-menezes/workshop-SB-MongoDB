4e7d73cd3f151441265b9f443a44b749b4a44490
junior-menezes <casagrandejuniorm@gmail.com>
2021-05-12 11:10:18 -0300

    Referenciando os Posts dos usuários

--- src/main/java/online/diagnosticit/workshopmongo/domain/User.java
+++ src/main/java/online/diagnosticit/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package online.diagnosticit.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //@Document(collection="user") pode ser utuilizada para setar o nome da tabela manualmente. Poderia ser "usuario"
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) //LAZY garante que ao buscar os usuários não vira junto os posts, a não ser que seja explicito.
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -47,6 +53,10 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 	@Override
