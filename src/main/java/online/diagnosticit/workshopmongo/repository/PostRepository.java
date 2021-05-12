7e98f76d9e96e150a4b6c6da0cd3393bdf91d03b
junior-menezes <casagrandejuniorm@gmail.com>
2021-05-12 17:20:58 -0300

    Consulta simples com query methods

--- src/main/java/online/diagnosticit/workshopmongo/repository/PostRepository.java
+++ src/main/java/online/diagnosticit/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package online.diagnosticit.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import online.diagnosticit.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/online/diagnosticit/workshopmongo/services/PostService.java
+++ src/main/java/online/diagnosticit/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package online.diagnosticit.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,8 @@ public class PostService {
 		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. ID: " + id));
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 
 }
